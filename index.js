9fc3b3b9a0dc9d3f754611e87e1d52409b3670e8
Al Abir Anik <alabiranik56@gmail.com>
2025-04-30 08:35:45 +0600

    made Game related Apis

--- index.js
+++ index.js
@@ -4,41 +4,77 @@ const cors = require("cors");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
-
 // Middlewires
 app.use(cors());
 app.use(express.json());
 
-
-// ujZUVVOXBZa4fSgd
-const uri = `mongodb+srv://alabirAnik:<db_password>@root-cluster.yqkit.mongodb.net/?retryWrites=true&w=majority&appName=root-Cluster`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// CONNECTION CODE START___
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@root-cluster.yqkit.mongodb.net/?retryWrites=true&w=majority&appName=root-Cluster`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    // Collection Names
+    const gamesCollection = client.db("PlayGrid_DB").collection("all_games");
+    
+
+    // ............Meal related APIs.............
+    // Add a new Meal
+    app.post("/all-games", async (req, res) => {
+      const newGame = req.body;
+      const result = await gamesCollection.insertOne(newGame);
+      res.send(result);
+    });
+     // load category games
+     app.get("/category-games", async (req, res) => {
+      const cursor = gamesCollection.find().limit(3);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // load specific meal
+    app.get("/game/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await gamesCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("Games is falling from the sky yoooooooooo");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Games is waiting at ${port}`);
-  });
+  res.send("Games is falling from the sky yoooooooooo");
+});
+
+app.listen(port, () => {
+  console.log(`Games is waiting at ${port}`);
+});
