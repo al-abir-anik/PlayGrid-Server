f20dd3ede4e34ac595b051ff4f2e547ce7d73efe
Al Abir Anik <alabiranik56@gmail.com>
2025-05-01 21:11:46 +0600

    Save local changes before pulling

--- index.js
+++ index.js
@@ -4,41 +4,36 @@ const cors = require("cors");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
-
 // Middlewires
 app.use(cors());
 app.use(express.json());
 
-
-// ujZUVVOXBZa4fSgd
-const uri = `mongodb+srv://alabirAnik:<db_password>@root-cluster.yqkit.mongodb.net/?retryWrites=true&w=majority&appName=root-Cluster`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@root-cluster.yqkit.mongodb.net/?retryWrites=true&w=majority&appName=root-Cluster`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 app.get("/", (req, res) => {
-    res.send("Games is falling from the sky yoooooooooo");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Games is waiting at ${port}`);
-  });
+  res.send("Games is falling from the sky yoooooooooo");
+});
+
+app.listen(port, () => {
+  console.log(`Games is waiting at ${port}`);
+});
